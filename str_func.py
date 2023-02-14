8b3e0c69de94dfeb369afc7f01a707580ef94639
aleksandrveselov <sanek_ves@mail.ru>
2023-02-14 22:51:29 +0300

    add function str_func2 in str_func.py/добавлена функция str_func2 в str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(string):
+    """возвращает строку в верхнем регистре"""
     return string.upper()
+
+
+def str_func2(string):
+    """return string in title"""
+    return string.title()
